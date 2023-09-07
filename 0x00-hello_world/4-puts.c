30205cd5fdcdb9f9cc42e34a35a38635d7298204
Yonas Negese <yonasnegese250@gmail.com>
2023-09-07 12:59:16 -0700

    My fifth pro

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main put function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
+
