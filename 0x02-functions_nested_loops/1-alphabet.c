352bc744d7410bd3d1b72c1688106f699374b6b0
Yonas Negese <yonasnegese250@gmail.com>
2023-09-13 05:32:10 -0700

    I sometimes suffer from insomnia.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,11 @@
  */
 void print_alphabet(void)
 {
-char apl;
-for (alp = 'a', alp <= 'z', alp++);
-_putchar(alp);
-_putchar('\n');
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+		_putchar(le);
+
+	_putchar('\n');
 }
+
