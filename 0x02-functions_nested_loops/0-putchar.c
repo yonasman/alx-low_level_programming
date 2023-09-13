9eab1f512aadaced43c9d7d159c2a5992ceb0557
Yonas Negese <yonasnegese250@gmail.com>
2023-09-13 05:03:47 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
-int main()
+#include "main.h"
+/**
+ * main - print _putchar
+ *author yonas negese
+ * Return: 0
+ */
+
+int main(void)
 {
-	char hey[] = "_putchar";
-	printf('%c\n', hey);
-	return(0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
