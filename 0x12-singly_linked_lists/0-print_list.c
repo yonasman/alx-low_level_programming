3ff512c3c45d984bb2c8f4fd78a221017a258da4
yonasman <yonasnegese250@gmail.com>
2023-10-19 11:35:56 -0700

    first linked lists pro

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *print_list - print all elements
+ *@h: a head of the list
+ *
+ *Return: Always 0
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%u] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *_putchar - prints a char
+ *
+ *Return:1 on success else -1
+ */
+int _putchar(void char)
+{
+	return(write(1, &c, 1));
+}
