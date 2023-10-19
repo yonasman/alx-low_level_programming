f575b0da7afea6dcaac8e7ee7a8ed4e508a9f352
yonasman <yonasnegese250@gmail.com>
2023-10-19 12:20:08 -0700

    linked list second pro

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *list_len - prints the number of elements
+ *@h: the head of the list
+ *
+ *Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+	for (i = 1; h->next != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
