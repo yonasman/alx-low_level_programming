0a5be025dd2c8fa3120460df708be5e3fb3e282c
yonasman <yonasnegese250@gmail.com>
2023-10-30 15:53:24 -0700

    first file I/O pro

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,28 +10,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buffer;
-	ssize_t rd, wr;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 	{
 		return (0);
 	}
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 	{
 		return (0);
 	}
-	rd = (fd, buffer, letters);
-	wr = (STDOUT_FILENO, buffer, rd);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
-	free(buffer);
 
-	return (wr);
+	free(buf);
+
+	return (nwr);
 }
